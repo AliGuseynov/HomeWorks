456f0a391deb94370136f717d71e48bc9a2d1e82
NigarEyvazli <nigarme@code.edu.az>
2022-12-16 12:19:26 +0400

    abc

---
+++ TaskDay4-BookStock/src/Task2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        String str = "matata";
+
+        char[] strArr = new char[str.length()];
+
+        for (int i = 0; i < strArr.length; i++)
+        {
+            strArr[i] = str.charAt(i);
+        }
+
+        Scanner inn = new Scanner(System.in);
+
+        char search = inn.next().charAt(0);
+
+        int counter = 0;
+
+        for (int i = 0; i < strArr.length; i++)
+        {
+            if(search == strArr[i]) counter++;
+        }
+
+        System.out.println(counter);
+    }
+}
