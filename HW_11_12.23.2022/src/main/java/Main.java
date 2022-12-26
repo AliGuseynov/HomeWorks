a926f1fe6d2caf27c2948eb74401ca648c6aa466
malishov <119285910+malishov@users.noreply.github.com>
2022-12-26 15:11:38 +0400

    Add files via upload

---
+++ HW_11_12.23.2022/src/main/java/Main.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+
+//        Task1.run();
+        Task2.run();
+
+
+    }
+}
