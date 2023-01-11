880208294f57836fbaccdb592abda9f2bd31e039
leylakhalilli <leylaxelilli65@gmail.com>
2023-01-11 23:35:54 +0400

    1/11/2023

---
+++ ForSQL/src/main/java/az/code/SQL/Main.java
@@ -0,0 +1,38 @@
+package az.code.SQL;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+//        task2
+
+//        select distinct city from station where city RLIKE '[^aeiouAEIOU]$';
+
+
+        //task3
+
+//        select p.firstName,p.lastName,a.city,a.state
+//        from Person p left join Address a
+//        on p.personId=a.personId;
+
+
+        //task 4
+//        select e.name as "Employee"
+//        from Employee e
+//        left join Employee a
+//        on e.managerId=a.id
+//        where e.salary > a.salary
+
+//        task5
+
+//        select name from Employee
+//        where salary > 2000 and months <10
+
+//        order by employee_id;//???
+
+
+
+
+
+    }
+}
