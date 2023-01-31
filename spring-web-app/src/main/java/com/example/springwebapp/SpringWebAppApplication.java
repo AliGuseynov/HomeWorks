3d64262aa489967d3b28f8f66991952d34d928f3
NezrinSuleymani <nazrinss@code.edu.az>
2023-01-31 10:55:47 +0400

    web task

---
+++ spring-web-app/src/main/java/com/example/springwebapp/SpringWebAppApplication.java
@@ -0,0 +1,22 @@
+package com.example.springwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebAppApplication.class, args);
+    }
+
+
+
+
+
+
+
+
+
+    +
+}
